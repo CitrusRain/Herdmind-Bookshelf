00520830519185f4e15d1aa74007aa99f46fddf9
Ryan Young <citrus@mint>
2014-02-19 20:22:03 -0500

    Starring system - adds state checker

--- Herdmind/_incl/RetreiveData.php
+++ Herdmind/_incl/RetreiveData.php
@@ -293,7 +293,7 @@ while ($line = mysqli_fetch_array($run, MYSQL_ASSOC)) {
 
 $starred = checkStar("submissionID", $facts[7]);
 
-if(!(empty($starred))
+if(!(empty($starred)))
 {	echo "starred on ".$starred[0].", last viewed on ".$starred[1]."<br/>";
 	$starbool = true;
 }
