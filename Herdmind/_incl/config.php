16a0b117cd160f54f3cacfc1c6adbe4f1c738513
Ryan <citrusrain@herdmind.net>
2014-02-20 20:28:07 -0500

    Update config.php
    Moved password location to an ignored file.

--- Herdmind/_incl/config.php
+++ Herdmind/_incl/config.php
@@ -1,40 +1,11 @@
 <?php
-/*
- * Initiates a database connection and provides a global variable $db_connection so other files can access it
- */
 
-
-/*
-$mysql_host = "localhost";
-$mysql_database = "citrus_mane";
-$mysql_user = "citrus_mane";
-$mysql_password = "trainwreck27";
-*/
-
-//These are the login details for the development database copy.
-$mysql_host = "localhost";
-$mysql_database = "citrus_BETABASE";
-$mysql_user = "citrus_develop";
-$mysql_password = "hejustcalledme";
-
-//Forum tables' prefix
-global $forumprefix;
-$forumprefix = "zforums";
-
-
-
-//Create database connection for website
-global $db_connection;
-$db_connection = mysqli_connect($mysql_host, $mysql_user, $mysql_password);
-mysqli_select_db($db_connection, $mysql_database);
-
-if (mysqli_connect_errno($db_connection)) {
-	echo "Error connection to database: " . mysqli_connect_error();
-}
+include $_SERVER['DOCUMENT_ROOT']."/_incl/.gitignore/mysqlconnect.php";
 
 //Set other global parameters 
 
 //These make sure comments are sorted.
+//I don't remember how. Might be depreciated.
 global $ThreadBoardID_Topics;
 global $ThreadBoardID_Fanfacts;
 global $ThreadBoardID_Fanworks;
