cfeb12f4689b36b548f1c6706e7c06e33936cd2a
CitrusRain <citrusrain@herdmind.net>
2014-03-18 18:35:51 -0400

    Renamed a file. Because my workspace did not like that name.

--- Herdmind/_incl/config.php
+++ Herdmind/_incl/config.php
@@ -1,6 +1,6 @@
 <?php
 $str = $_SERVER['DOCUMENT_ROOT']."/_incl/";
-echo "<a href='$str'>$str</a> .gitignore/connectToServer.php";
+
 //include $_SERVER['DOCUMENT_ROOT']."/_incl/.gitignore/mysqlconnect.php";
 include dirname(__FILE__)."/.gitignore/connectToServer.php";
 //echo "<hr>";
