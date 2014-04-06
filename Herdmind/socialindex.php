a0c88096aeba655838bdac8bbe58b3ead5633499
CitrusRain <citrusrain@herdmind.net>
2014-04-06 17:34:57 -0400

    Can't make it work.

--- Herdmind/socialindex.php
+++ Herdmind/socialindex.php
@@ -218,7 +218,7 @@ Make a place to make a thread with
 echo "
 <form method='POST'>
 		<textarea id='commentbox'></textarea>
-		<button type='button' onclick='PostNew(\"".$fandom->fandomid."\")'>Submit a Shitpost</button>
+		<button type='button' onclick='PostNew(\"".$fandom->fandomid."\")'>Submit a Post</button>
 	</form>
 ";
 
