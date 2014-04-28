c6f337bd286cf0edf0af1cb1e43335853a4104c7
Sharuma <gr33nmous3@gmail.com>
2014-04-28 09:25:19 +0530

    Project to rewrite the entire site to use templating rather than embedded HTML

---
+++ Herdmind/.include/global.php
@@ -0,0 +1,8 @@
+<?php
+  session_start();
+  require dirname(__FILE__)."/herdmind.class.php";
+  require dirname(__FILE__)."/fanbase.class.php";
+  require dirname(__FILE__)."/xtemplate.class.php";
+
+  define("TEMPLATE_DIR", dirname(__FILE__)."/template");
+?>
