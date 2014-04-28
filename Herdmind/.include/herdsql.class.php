d32edfbbe2ab5e27a16c26d51376f1f4f591333b
Sharuma <gr33nmous3@gmail.com>
2014-04-28 10:10:14 +0530

    HerdSQL

---
+++ Herdmind/.include/herdsql.class.php
@@ -0,0 +1,65 @@
+<?php
+
+class SQL {
+  function __construct($host, $user, $pass, $db, $port) {
+    $this->socket = NULL;
+
+    if(is_object($host) && get_class($host) == "mysqli") {
+      $this->socket = $host;
+    } else {
+      $this->socket = new mysqli($host, $user, $pass, $db, $port);
+    }
+
+    if($this->socket->connect_errno) {
+      throw new Exception($this->socket->connect_error);
+    }
+  }
+
+  function __destruct() {
+    if($this->socket != NULL) {
+      $this->socket->close();
+    }
+  }
+
+  //This will cause an exception to be raised if the socket is invalid
+  public function alive() {
+    if($this->socket == NULL || !is_object($this->socket) || !get_class($this->socket) == "mysqli") {
+      throw new Exception("MySQLi socket is closed! ".$this->socket->connect_error);
+    }
+  }
+
+  public functoin generate($sql, $func, $args) {
+    $qry = array();
+
+    $class = new ReflectionClass(self);
+    $method = $class->getMethod($func);
+
+    foreach($method->getParameters() as $param) {
+      $qry[] = "<% " . $param . " %>";
+    }
+
+    return str_replace($qry, $args, $sql);
+  }
+
+  private static $POPULAR_FACTS = file_get_contents(dirname(__FILE__) . "/sql/popular_facts.sql");
+
+  public function getPopularFacts($iId, $bMature) {
+    $this->alive();
+
+    $sql = $this->generate(self::POPULAR_FACTS, "getPopularFacts", func_get_args());
+
+    if($result = $this->socket->query($sql)) {
+      var $arr = array();
+      while(($row = $result->fetch_array(MYSQLI_BOTH)) !== NULL) {
+        $arr[] = $row;
+      }
+      return $arr;
+    } else if($this->socket->errno) {
+      throw new Exception($this->socket->error);
+    } else {
+      return array();
+    }
+  }
+}
+
+?>
---
+++ Herdmind/.include/sql/popular_fanfacts.sql
@@ -0,0 +1,44 @@
+SELECT * FROM
+(
+  SELECT DISTINCT Fact.FactID, Fact.DatePosted, Fact.Contents, sum(tal.Value) as RecentSum FROM
+  (
+    (
+      (
+        (
+          Fact LEFT JOIN SubmissionData AS s ON FactID = s.SubmissionID
+        )
+        LEFT JOIN FactScoreByTally AS tal ON Fact.FactID = tal.FactID
+      )
+      JOIN FactBranch AS fb ON Fact.FactID = fb.FactID
+    )
+    JOIN Branch AS b ON fb.BranchID = b.BranchID
+  )
+  WHERE b.branchid = '<% iId %>'
+  AND tal.DatePointScored > DATE_SUB(curdate(), INTERVAL 2 WEEK)
+  AND s.IsPublic='1'
+  AND s.IsMature='<% bMature %>'
+  AND s.IsRemoved='0'
+  GROUP BY Fact.FactID
+  ORDER BY sum(tal.Value)
+  DESC LIMIT 10
+) AS PopularFacts LEFT JOIN (
+  SELECT DISTINCT Fact.FactID, sum(tal.Value) as TotalSum, s.ID FROM
+  (
+    (
+      (
+        (
+          Fact LEFT JOIN SubmissionData AS s ON FactID = s.SubmissionID
+        )
+        LEFT JOIN FactScoreByTally AS tal ON Fact.FactID = tal.FactID
+      )
+      JOIN FactBranch AS fb ON Fact.FactID = fb.FactID
+    )
+    JOIN Branch AS b ON fb.BranchID = b.BranchID
+  )
+  WHERE b.branchid = '<% iId %>'
+  AND s.IsPublic='1'
+  AND s.IsMature='<% bMature %>'
+  AND s.IsRemoved='0'
+  GROUP BY Fact.FactID
+) AS AllTimePopularFacts ON PopularFacts.FactID = AllTimePopularFacts.FactID
+ORDER BY Rand()
